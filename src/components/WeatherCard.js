5f513b7fabd6bf42dea6453b72a6b788911285d2
Zuke27 <wstaylor27@gmail.com>
2022-04-30 14:57:04 -0400

    Added components to card

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,23 +1,27 @@
 import React from "react";
-import { Card, Icon, Image } from "semantic-ui-react";
+import { Card, Feed, Icon, Image } from "semantic-ui-react";
 
 const WeatherCard = () => (
   <Card>
-    <Image src="/images/avatar/large/matthew.png" wrapped ui={false} />
     <Card.Content>
-      <Card.Header>Matthew</Card.Header>
-      <Card.Meta>
-        <span className="date">Joined in 2015</span>
-      </Card.Meta>
-      <Card.Description>
-        Matthew is a musician living in Nashville.
-      </Card.Description>
+      <Card.Header>City</Card.Header>
     </Card.Content>
-    <Card.Content extra>
-      <a>
-        <Icon name="user" />
-        22 Friends
-      </a>
+    <Card.Content>
+      <Feed>
+        <Feed.Event>
+          <Feed.Content>
+            <div className="weather-card">
+              <div className="weather-card-child">Temperature</div>
+              <div className="weather-card-child">Humidity</div>
+            </div>
+
+            <div className="weather-card">
+              <div className="weather-card-child">Sunrise</div>
+              <div className="weather-card-child">Sunset</div>
+            </div>
+          </Feed.Content>
+        </Feed.Event>
+      </Feed>
     </Card.Content>
   </Card>
 );

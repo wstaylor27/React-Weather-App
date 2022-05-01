b38ef0ff37a8b7ce1341ffa13470ba0f196703f0
Zuke27 <wstaylor27@gmail.com>
2022-05-01 15:45:34 -0400

    Added API key

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import WeatherCard from "./components/WeatherCard";
 import React from "react";
 
+const URL = "https://api.openweathermap.org/data/2.5/onecall";
+const API_KEY = "452dc8628e72dae5457b9f213f5b4fb2";
+
 function App() {
+  useEffect(() => {}, []);
   return (
     <div className="main">
       <Header />

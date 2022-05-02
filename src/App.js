0e4996231a1637ade81a042103b3a2c4ff322e68
Zuke27 <wstaylor27@gmail.com>
2022-05-01 20:03:11 -0400

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,55 @@
-import { useEffect } from "react";
-import "./App.css";
-import Header from "./components/Header";
-import WeatherCard from "./components/WeatherCard";
-import React from "react";
-
-const URL = "https://api.openweathermap.org/data/2.5/onecall";
-const API_KEY = "452dc8628e72dae5457b9f213f5b4fb2";
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
-  useEffect(() => {}, []);
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=b48d5786d2dfa111347327b98152c6b3`;
+
+  const searchLocation = (event) => {
+    if (event.key === "Enter") {
+      axios.get(url).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      });
+      setLocation("");
+    }
+  };
+
   return (
-    <div className="main">
-      <Header />
-      <WeatherCard />
+    <div className="app">
+      <div className="search">
+        <input
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder="Enter Location"
+          type="text"
+        />
+      </div>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Dallas</p>
+          </div>
+          <div className="temp">
+            <h1>60°F</h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+          <div className="bottom">
+            <div className="feels">
+              <p>65°F</p>
+            </div>
+            <div className="humidity">
+              <p>20%</p>
+            </div>
+            <div className="wind">12 MPH</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
